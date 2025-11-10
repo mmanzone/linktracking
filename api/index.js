43a4a0b1f8e11d489a0e9d32d68c0a7ac218e242
mmanzone <70729568+mmanzone@users.noreply.github.com>
2025-11-10 15:24:17 +1100

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Redis } = require('@upstash/redis');
-const path = require('path');
 
 const app = express();
 
