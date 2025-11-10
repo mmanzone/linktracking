f8636be587beee75977a6cd6aafca8c2d9656d09
mmanzone <70729568+mmanzone@users.noreply.github.com>
2025-11-10 14:35:03 +1100

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const app = express();
 
 // --- Upstash Redis Client Setup ---
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 });
 
 // --- Data Initialization ---
